2cc0d99997c832ec0c3ee5970cb3ff981122048b
Dmytro <samui2006@ukr.net>
2017-04-13 10:13:27 +0300

    My Commit

---
+++ CrunchBaseReady/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace CrunchBaseReady
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.uploadExcelBtn = new System.Windows.Forms.Button();
+            this.excelColumnBtn = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.clmnNames = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.xprtToXls = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // uploadExcelBtn
+            // 
+            this.uploadExcelBtn.Location = new System.Drawing.Point(97, 45);
+            this.uploadExcelBtn.Name = "uploadExcelBtn";
+            this.uploadExcelBtn.Size = new System.Drawing.Size(121, 23);
+            this.uploadExcelBtn.TabIndex = 0;
+            this.uploadExcelBtn.Text = "Upload";
+            this.uploadExcelBtn.UseVisualStyleBackColor = true;
+            this.uploadExcelBtn.Click += new System.EventHandler(this.uploadExcelBtn_Click);
+            // 
+            // excelColumnBtn
+            // 
+            this.excelColumnBtn.Location = new System.Drawing.Point(97, 133);
+            this.excelColumnBtn.Name = "excelColumnBtn";
+            this.excelColumnBtn.Size = new System.Drawing.Size(121, 23);
+            this.excelColumnBtn.TabIndex = 1;
+            this.excelColumnBtn.Text = "Bring All Data";
+            this.excelColumnBtn.UseVisualStyleBackColor = true;
+            this.excelColumnBtn.Click += new System.EventHandler(this.excelColumnBtn_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(94, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "STEP 1 - Upload Excel";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(94, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(200, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "STEP 2 - Which field is the company url?";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(400, 29);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(687, 381);
+            this.listBox1.TabIndex = 7;
+            // 
+            // clmnNames
+            // 
+            this.clmnNames.FormattingEnabled = true;
+            this.clmnNames.Location = new System.Drawing.Point(97, 106);
+            this.clmnNames.Name = "clmnNames";
+            this.clmnNames.Size = new System.Drawing.Size(121, 21);
+            this.clmnNames.TabIndex = 8;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(97, 194);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Export";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // xprtToXls
+            // 
+            this.xprtToXls.AutoSize = true;
+            this.xprtToXls.Location = new System.Drawing.Point(94, 178);
+            this.xprtToXls.Name = "xprtToXls";
+            this.xprtToXls.Size = new System.Drawing.Size(130, 13);
+            this.xprtToXls.TabIndex = 10;
+            this.xprtToXls.Text = "STEP 3 - Export your data";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1110, 444);
+            this.Controls.Add(this.xprtToXls);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.clmnNames);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.excelColumnBtn);
+            this.Controls.Add(this.uploadExcelBtn);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uploadExcelBtn;
+        private System.Windows.Forms.Button excelColumnBtn;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ComboBox clmnNames;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label xprtToXls;
+    }
+}
+
---
+++ CrunchBaseReady/Form1.cs
@@ -0,0 +1,353 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+//using GemBox.Spreadsheet;
+using excel = Microsoft.Office.Interop.Excel;
+using Microsoft.Office.Interop.Excel;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Support.UI;
+using System.Text.RegularExpressions;
+using System.Runtime.InteropServices;
+
+namespace CrunchBaseReady
+{
+    public partial class Form1 : Form
+    {
+
+        IWebDriver driver;
+
+        OpenFileDialog openFile;
+        excel.Application excelApp;
+        excel.Workbook wb;
+        excel.Worksheet ws;
+        excel.Application excelAppExp = new excel.Application();
+        object misValue = System.Reflection.Missing.Value;
+        List<string> alldata = new List<string>();
+        List<string> rowsNames = new List<string>();
+        string currJobPage = null;
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void uploadExcelBtn_Click(object sender, EventArgs e)
+        {
+            openFile = new OpenFileDialog();
+            excelApp = new excel.Application();
+            if (openFile.ShowDialog() == DialogResult.OK)
+            {
+                excel.Workbooks excelWB = excelApp.Workbooks;
+                excelWB.Open(openFile.FileName);
+                excel._Worksheet sheets = excelApp.ActiveSheet;
+
+                int columnCount = sheets.UsedRange.Columns.Count;
+                List<string> columnNames = new List<string>();
+                for (int c = 1; c < columnCount; c++)
+                {
+                    if (sheets.Cells[1, c].Value2 != null)
+                    {
+                        string columnName = sheets.Columns[c].Address;
+                        Regex reg = new Regex(@"(\$)(\w*):");
+                        if (reg.IsMatch(columnName))
+                        {
+                            Match match = reg.Match(columnName);
+                            columnNames.Add(match.Groups[2].Value);
+                        }
+                    }
+                }
+                foreach (var item in columnNames)
+                {
+                    clmnNames.Items.Add(item);
+                }
+            }
+        }
+
+
+
+        private void excelColumnBtn_Click(object sender, EventArgs e)
+        {
+
+
+
+            //////////////////////////////////////////////////////
+            excel._Worksheet sheets = excelApp.ActiveSheet;
+            excel.Range last = sheets.Cells.SpecialCells(excel.XlCellType.xlCellTypeLastCell, Type.Missing);
+            excel.Range range = sheets.get_Range("A1", last);
+            var iTotalRows = sheets.UsedRange.Rows.Count;
+
+            int row = 1;
+
+            driver = new FirefoxDriver();
+            //Loop get all Urls in column
+            for (int rows = 1; rows < iTotalRows - 1; rows++)
+            {
+                //
+                driver.Navigate().GoToUrl(sheets.Cells[row++, clmnNames.Text].value);
+                driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(5.00));
+                IWebElement elem = driver.FindElement(By.XPath(".//*[@id='profile_header_heading']/a"));
+                string s = elem.Text;
+                listBox1.Items.Add(s); // Company Name
+                alldata.Add(s);
+
+
+
+                IList<IWebElement> founderList = driver.FindElements(By.XPath(".//*[@id='info-card-overview-content']/div/dl/div[2]/dd[3]/a"));
+                IList<IWebElement> founderList1 = driver.FindElements(By.XPath(".//*[@id='info-card-overview-content']/div/dl/div[2]/dd[3]/a"));
+
+                List<string> urls = new List<string>();
+                List<string> founderName = new List<string>();
+
+                foreach (var item in founderList)
+                {
+                    urls.Add(item.GetAttribute("href"));
+
+
+                }
+                foreach (var item in founderList)
+                {
+                    founderName.Add(item.Text);
+                }
+
+                bool present;
+                foreach (var item2 in urls)
+                {
+
+
+                    driver.Navigate().GoToUrl(item2);
+                    driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(3.00));
+                    //1////////////////////////////////////////////////////founder Name//////////////////////////////////////////////////////////////////////////
+                    listBox1.Items.Add(founderName);
+                    alldata.Add(founderName.ToString());
+                    //2////////////////////////////////////////////////////Founder crunch Page//////////////////////////////////////////////////////////////////////////
+                    listBox1.Items.Add(urls);
+                    alldata.Add(urls.ToString());
+
+                    //3////////////////////////////////////////////////////Role//////////////////////////////////////////////////////////////////////////
+                    try
+                    {
+                        driver.FindElement(By.XPath(".//*[@id='info-card-overview-content']/div/dl/div/dd[1]"));
+                        listBox1.Items.Add(driver.FindElement(By.XPath(".//*[@id='info-card-overview-content']/div/dl/div/dd[1]")).Text); // Role
+                        alldata.Add(driver.FindElement(By.XPath(".//*[@id='info-card-overview-content']/div/dl/div/dd[1]")).Text);
+                        present = true;
+                    }
+                    catch (NoSuchElementException)
+                    {
+                        listBox1.Items.Add("Role element doesn't exist");
+                        alldata.Add("Role element doesn't exist");
+                        present = false;
+                    }
+
+                    //4////////////////////////////////////////////////////# of investments//////////////////////////////////////////////////////////////////////////
+                    if (driver.FindElements(By.XPath(".//*[@id='info-card-overview-content']/div/dl/div/dd[2]/a")).Any())
+                    {
+                        listBox1.Items.Add(driver.FindElement(By.XPath(".//*[@id='info-card-overview-content']/div/dl/div/dd[2]/a")).Text); //# of investments
+                        alldata.Add(driver.FindElement(By.XPath(".//*[@id='info-card-overview-content']/div/dl/div/dd[2]/a")).Text);
+                    }
+                    else
+                    {
+                        listBox1.Items.Add("Element doesn't exist.");
+                        alldata.Add("# of investments element doesn't exist");
+                    }
+                    //5////////////////////////////////////////////////////Gender//////////////////////////////////////////////////////////////////////////
+                    if (driver.FindElements(By.XPath(".//*[@id='info-card-overview-content']/div/dl/dd[2]")).Any())
+                    {
+                        listBox1.Items.Add(driver.FindElement(By.XPath(".//*[@id='info-card-overview-content']/div/dl/dd[2]")).Text); //Gender
+                        alldata.Add(driver.FindElement(By.XPath(".//*[@id='info-card-overview-content']/div/dl/dd[2]")).Text);
+                    }
+                    else
+                    {
+                        listBox1.Items.Add("Element doesn't exist.");
+                        alldata.Add("Gender element doesn't exist.");
+                    }
+                    //6////////////////////////////////////////////////////Location//////////////////////////////////////////////////////////////////////////
+                    if (driver.FindElements(By.XPath(".//*[@id='info-card-overview-content']/div/dl/dd[3]")).Any())
+                    {
+                        listBox1.Items.Add(driver.FindElement(By.XPath(".//*[@id='info-card-overview-content']/div/dl/dd[3]")).Text);//Location
+                        alldata.Add(driver.FindElement(By.XPath(".//*[@id='info-card-overview-content']/div/dl/dd[3]")).Text);
+                    }
+                    else
+                    {
+                        listBox1.Items.Add("Element doesn't exist.");
+                        alldata.Add("Element location doesn't exist.");
+                    }
+                    //7//////////////////////////////////////////////////WebSite//////////////////////////////////////////////////////////////////////////
+                    if (driver.FindElements(By.XPath(".//*[@id='info-card-overview-content']/div/dl/dd[4]/a")).Any())
+                    {
+                        listBox1.Items.Add(driver.FindElement(By.XPath(".//*[@id='info-card-overview-content']/div/dl/dd[4]/a")).Text);//Website
+                        alldata.Add(driver.FindElement(By.XPath(".//*[@id='info-card-overview-content']/div/dl/dd[4]/a")).Text);
+                    }
+                    else
+                    {
+                        listBox1.Items.Add("Element doesn't exist.");
+                        alldata.Add("Element Website doesn't exist.");
+                    }
+                    //8//9//10////////////////////////////////////////Social//////////////////////////////////////////////////////////////////////////
+                    if (driver.FindElements(By.XPath(".//*[@id='info-card-overview-content']/div/dl/dd[5]/a")).Any())
+                    {
+                        List<IWebElement> socials = driver.FindElements(By.XPath(".//*[@id='info-card-overview-content']/div/dl/dd[5]/a")).ToList();
+                        List<string> socialsUrls = new List<string>();
+                        foreach (var item in socials)
+                        {
+                            socialsUrls.Add(item.GetAttribute("href"));
+                        }
+                        foreach (var item in socialsUrls)
+                        {
+                            listBox1.Items.Add(item);//Social
+                            alldata.Add(item);
+                        }
+                    }
+                    else
+                    {
+                        listBox1.Items.Add("Element doesn't exist.");
+                        alldata.Add("Element Website doesn't exist.");
+                    }
+                    //11////////////////////////////////////////////////Personal details//////////////////////////////////////////////////////////////////////////
+                    if (driver.FindElements(By.ClassName("description-ellipsis")).Any())
+                    {
+                        List<IWebElement> personal = driver.FindElements(By.ClassName("description-ellipsis")).ToList();
+                        List<string> personalText = new List<string>();
+
+                        foreach (var item in personal)
+                        {
+                            personalText.Add(item.Text);
+                        }
+
+                        foreach (var item in personalText)
+                        {
+                            listBox1.Items.Add(item);
+                            alldata.Add(item);
+                        }
+
+                    }
+                    else
+                    {
+                        listBox1.Items.Add("Element personal details doesn't exist.");
+                        alldata.Add("Element personal details doesn't exist.");
+                    }
+                    //12//////////////////////////////////////////////////Current Job Company//////////////////////////////////////////////////////////////////////////
+                    if (driver.FindElements(By.XPath(".//*[@id='main-content']/div[2]/div[2]/div[3]/div[4]/div[2]/div[2]/div[2]/h5[1]/a")).Any())
+                    {
+                        listBox1.Items.Add(driver.FindElement(By.XPath(".//*[@id='main-content']/div[2]/div[2]/div[3]/div[4]/div[2]/div[2]/div[2]/h5[1]/a")).Text);//Current job Company
+                        alldata.Add(driver.FindElement(By.XPath(".//*[@id='main-content']/div[2]/div[2]/div[3]/div[4]/div[2]/div[2]/div[2]/h5[1]/a")).Text);
+                    }
+                    else
+                    {
+                        listBox1.Items.Add("Element Current Job doesn't exist.");
+                        alldata.Add("Element Current Job doesn't exist.");
+                    }
+                    //13//////////////////////////////////////////////////Current Job Company Crunch URL/////////////////////////////////////////////////////////////////
+                    if (driver.FindElements(By.XPath(".//*[@id='main-content']/div[2]/div[2]/div[3]/div[4]/div[2]/div[2]/div[2]/h5/a")).Any())
+                    {
+                        listBox1.Items.Add(driver.FindElement(By.XPath(".//*[@id='main-content']/div[2]/div[2]/div[3]/div[4]/div[2]/div[2]/div[2]/h5/a")).GetAttribute("href"));//Current job Company CrunchUrl
+                        alldata.Add(driver.FindElement(By.XPath(".//*[@id='main-content']/div[2]/div[2]/div[3]/div[4]/div[2]/div[2]/div[2]/h5/a")).GetAttribute("href"));
+                    }
+                    else
+                    {
+                        listBox1.Items.Add("Element Current Job doesn't exist.");
+                        alldata.Add("Element Current Job doesn't exist.");
+                    }
+                    //14//////////////////////////////////////////////////Current Job Company WebSite/////////////////////////////////////////////////////////////////
+                    
+                    if (driver.FindElements(By.XPath(".//*[@id='main-content']/div[2]/div[2]/div[3]/div[4]/div[2]/div[2]/div[2]/h5/a")).Any())
+                    {
+                        currJobPage = driver.FindElement(By.XPath(".//*[@id='main-content']/div[2]/div[2]/div[3]/div[4]/div[2]/div[2]/div[2]/h5/a")).GetAttribute("href");//Current job Company CrunchUrl
+                        driver.Navigate().GoToUrl(currJobPage);
+                        if(driver.FindElements(By.XPath(".//*[@id='info-card-overview-content']/div/dl/div[2]/dd[5]/a")).Any())
+                        {
+                            listBox1.Items.Add(driver.FindElement(By.XPath(".//*[@id='info-card-overview-content']/div/dl/div[2]/dd[5]/a")).GetAttribute("href"));
+                            alldata.Add(driver.FindElement(By.XPath(".//*[@id='info-card-overview-content']/div/dl/div[2]/dd[5]/a")).GetAttribute("href"));
+                        }
+                        else
+                        {
+                            listBox1.Items.Add("Element Current Job Page doesn't exist.");
+                            alldata.Add("Element Current Job Page doesn't exist.");
+                        }
+                        
+                    }
+                    else
+                    {
+                        listBox1.Items.Add("Element Current Job Page doesn't exist.");
+                        alldata.Add("Element Current Job Page doesn't exist.");
+                    }
+                    ////////////////////////////////////////////////////End of page parse//////////////////////////////////////////////////////////////////////////
+                    listBox1.Items.Add("-----------------------------------------------");
+                }
+
+                //wb = excelAppExp.Workbooks.Add(misValue);
+                //ws = (excel.Worksheet)wb.Worksheets.get_Item(1);
+
+                //rowsNames.Add("Investor Name");
+                //rowsNames.Add("Investor Crunchbase Page");
+                //rowsNames.Add("Primary Role");
+                //rowsNames.Add("# Of Investments");
+                //rowsNames.Add("Gender");
+                //rowsNames.Add("Location");
+                //rowsNames.Add("Website");
+                //rowsNames.Add("LinkedIn");
+                //rowsNames.Add("Facebook");
+                //rowsNames.Add("ITwitter");
+                //rowsNames.Add("Person details");
+                //rowsNames.Add("Current Job Company");
+                //rowsNames.Add("Current Job Company crunchbase URl Page");
+                //rowsNames.Add("Current Job company website");
+
+                //int alldatarow = 1;
+                //for (int i = 1; i <= founderName.Count; i++)
+                //{
+                //    for (int j = 0; j < 14; j++)
+                //    {
+                //        ws.Cells[j, 1] = rowsNames;
+                //        //ws.Cells[j, 2] = alldata;
+                //    }
+                //}
+
+
+            }
+            driver.Quit();
+            excelApp.Quit();
+            listBox1.Items.Add("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx");
+            foreach (var item in alldata)
+            {
+                listBox1.Items.Add(alldata.ToList());
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            wb.SaveAs("d:\\crunchbase.xls", excel.XlFileFormat.xlWorkbookNormal, misValue, misValue, misValue, misValue, excel.XlSaveAsAccessMode.xlExclusive, misValue, misValue, misValue, misValue, misValue);
+
+
+
+            wb.Close(true, misValue, misValue);
+            excelAppExp.Quit();
+
+            Marshal.ReleaseComObject(ws);
+            Marshal.ReleaseComObject(wb);
+            Marshal.ReleaseComObject(excelAppExp);
+
+            MessageBox.Show("Excel file created , you can find it!");
+
+
+        }
+
+
+    }
+}
+
+
+/////////////////////////
+
